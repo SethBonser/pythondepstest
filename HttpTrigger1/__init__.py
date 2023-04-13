519caa664617b2424d8c5316fdf6a95610ea5a1e
Seth Bonser <sbonser34@gmail.com>
2023-04-13 15:41:21 -0600

    initial commit

---
+++ HttpTrigger1/__init__.py
@@ -0,0 +1,28 @@
+import logging
+from pip._internal.operations.freeze import freeze
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+        for requirement in freeze(local_only=True):
+            logging.info(requirement)
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
